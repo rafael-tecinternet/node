517d55c073e585cd06cb0c736a6bdcdb5dc5db43
Rafael Augusto <103936985+rafael-tecinternet@users.noreply.github.com>
2022-08-16 11:54:40 -0300

    node

--- exemplo4/servidor.js
+++ exemplo4/servidor.js
@@ -1,2 +1,22 @@
 import http from 'http'; /* importando modulo HTTP nativo do Node (existe a partir da instalação do node na máquina). Pegar as requisições de URL. */
-import fs from 'fs/promises';
+import fs from 'fs/promises'; /* JS Assincrono (operações independentes, ocorrem em paralelo). FS = File System */
+
+const monitorRequisicao = (requisicao, resposta) => /* arrow function */ {
+    resposta.writeHead(200, {"Content-Type":"text/html; charset=utf8"})
+    switch (requisicao.url) {
+        case '/':
+            fs.readFile("paginas/index.html").then(conteudo => resposta.end(conteudo));
+            break;
+        case '/sobre':
+            fs.readFile("paginas/sobre.html").then(conteudo => resposta.end(conteudo));
+            break;    
+        default:
+            resposta.writeHead(404);
+            fs.readFile("paginas/404.html").then(conteudo => resposta.end(conteudo));
+            break;
+    }
+}
+/* Criando um servidor baseado no módulo http que importamos e monitorando ele */
+const servidor = http.createServer(monitorRequisicao);
+/* Iniciando o servidor */
+servidor.listen(8080, () => {console.log('Servidor rodando...');});
---
+++ exemplo5/index.js
@@ -0,0 +1,11 @@
+let aluno = "rafa";
+let idade = 15;
+import chalk from 'chalk';
+console.log(chalk.magenta('Trabalhando com NPM'));
+if (idade >= 18) {
+    console.log(chalk.blue(`${aluno} é maior de idade!`))
+} else {
+    console.log(`${aluno} ${chalk.red('é menor de idade!')}`);
+}
+/* para instalar o chalk utilizamos o comando npm install chalk.
+Chalk é um dos muitos pacotes disponíveis no repositório npmjs.com */
